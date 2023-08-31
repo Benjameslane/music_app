9ba5394ab22a8ecf55e8fc6364a04341c3cdded2
Benjameslane <ben.james.lane9>
2023-08-31 11:57:38 -0600

    added table to browser

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import MusicTable from './Components/Music_Table/MusicTable';
+
 
 function App() {
+
+    const [songs, setSongs] = useState([]);
+
+  useEffect(() => {
+
+      getAllSongs();
+      console.log('hello world');
+   
+  }, []);
+
+
+  async function getAllSongs(){
+
+    const response = await axios.get('https://localhost:7246/api/Musics');
+    console.log(response.data);
+    setSongs(response.data)
+    //debugger
+  }
+
   return (
+   <div>
+
+    {/* <button onClick={() => getAllSongs()}>Get All Songs</button> */}
+
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Music Library</h1>
       </header>
+      <main>
+        <MusicTable />
+      </main>
     </div>
+
+   </div>
   );
 }
 
